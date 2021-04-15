bb2c9d9487d6e884be685ea4520662d6b63c8733
Rahul Yedida <yrahul3910@gmail.com>
2021-04-15 16:12:23 -0400

    Fixed header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 import Main from './components/Main';
 import 'normalize.css';
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="header">
+    <header className="header">
       <a href="/"><img alt="Logo" src="assets/logo.svg" /></a>
       <div className="links">
         <a href="/search?q=javascript">Search</a>
         <a href="#how-it-works">How it works</a>
         <a href="#about">About</a>
       </div>
-    </div>
+    </header>
   );
 }
 
