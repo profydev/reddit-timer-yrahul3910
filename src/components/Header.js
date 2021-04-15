dd6cb9fca76f62babdcd258f783a805c6dcfa807
Rahul Yedida <yrahul3910@gmail.com>
2021-04-15 19:11:00 -0400

    Added tests, fixed minor bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
-      <a href="/"><img alt="Logo" src="assets/logo.svg" /></a>
-      <div className="links">
-        <a href="/search?q=javascript">Search</a>
-        <a href="#how-it-works">How it works</a>
-        <a href="#about">About</a>
-      </div>
+      <Link to="/"><img alt="Logo" src="assets/logo.svg" /></Link>
+      <nav className="links">
+        <Link to="/search/javascript">Search</Link>
+        <Link to="/#how-it-works">How it works</Link>
+        <Link to="/#about">About</Link>
+      </nav>
     </header>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Header from './Header';
 
 function Home() {
   return (
-    <Header />
+    <p>Home page</p>
   );
 }
 
