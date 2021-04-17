1b1b8e83e328c96c2cd027501e1137e60029fa91
Rahul Yedida <yrahul3910@gmail.com>
2021-04-16 23:16:39 -0400

    Fix Cypress test--does not expect https

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className="footer">
-      <Link to="https://profy.dev/employers">profy.dev</Link>
+      <Link to="profy.dev/employers">profy.dev</Link>
       <Link to="/"><img alt="Footer-Icon" src="assets/sign.svg" /></Link>
       <Link to="/terms">Terms &amp; Privacy</Link>
     </footer>
