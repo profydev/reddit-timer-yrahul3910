63fc06872c7cac3994a3e22d948b05b375928335
Rahul Yedida <yrahul3910@gmail.com>
2021-04-16 23:20:53 -0400

    My battle with Cypress continues...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className="footer">
-      <Link to="profy.dev/employers">profy.dev</Link>
+      <a href="https://profy.dev/employers">profy.dev</a>
       <Link to="/"><img alt="Footer-Icon" src="assets/sign.svg" /></Link>
       <Link to="/terms">Terms &amp; Privacy</Link>
     </footer>
