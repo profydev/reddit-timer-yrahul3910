26c56d3a712daa7047659837dbb7c572890a6c8a
Rahul Yedida <yrahul3910@gmail.com>
2021-04-25 08:33:58 -0400

    Refactored Home styles into styled component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as S from '../styles/Home.style';
 
 function Home() {
   return (
-    <main>
-      <p className="title">No reactions to your reddit posts?</p>
-      <p className="subtitle">Great timing, great results! Find the best time to post on your subreddit.</p>
-      <Link to="/search?q=javascript"><button type="button">Show me the best time</button></Link>
+    <S.Main>
+      <S.Title>No reactions to your reddit posts?</S.Title>
+      <S.Subtitle>
+        Great timing, great results! Find the best time to post on your subreddit.
+      </S.Subtitle>
+      <Link to="/search?q=javascript"><S.Button>Show me the best time</S.Button></Link>
       <p id="subreddit">r/javascript</p>
       <Link to="/search?q=javascript"><img alt="table" src="assets/table.svg" /></Link>
-    </main>
+    </S.Main>
   );
 }
 
