7c4b2a452f8904024379778b7fd53617c709ce18
Rahul Yedida <yrahul3910@gmail.com>
2021-04-25 08:37:24 -0400

    Hero section (#4)
    * Completed tasks
    
    * Implemented header component
    
    * Fixed header
    
    * Added tests, fixed minor bugs
    
    * Added test assets folder, implemented Footer, fixed Header positioning
    
    * Fix for test
    
    * Fix Cypress test
    
    * Fix Cypress test--does not expect https
    
    * My battle with Cypress continues...
    
    * Added tests for Footer component
    
    * Added title and button to home page
    
    * Added table, fixed footer spacing
    
    * Refactored tests, added tests for Home component
    
    * Refactored Home styles into styled component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as S from '../styles/Home.style';
 
 function Home() {
   return (
-    <p>Home page</p>
+    <S.Main>
+      <S.Title>No reactions to your reddit posts?</S.Title>
+      <S.Subtitle>
+        Great timing, great results! Find the best time to post on your subreddit.
+      </S.Subtitle>
+      <Link to="/search?q=javascript"><S.Button>Show me the best time</S.Button></Link>
+      <p id="subreddit">r/javascript</p>
+      <Link to="/search?q=javascript"><img alt="table" src="assets/table.svg" /></Link>
+    </S.Main>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,21 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import theme from '../styles/theme';
 
 function Main() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/search">Search page</Route>
       </Switch>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
