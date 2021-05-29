357d0307e93bdae20c6b8646aba2f5d3d4665ffb
Rahul Yedida <rahul.yedida@pm.me>
2021-05-28 23:15:31 -0400

    Merge commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home/Home';
-import Header from './Header';
+import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import theme from '../theme';
 
