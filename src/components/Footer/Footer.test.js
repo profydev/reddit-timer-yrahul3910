22c14124634cf705c29fd65296cffa58210d5e3b
Rahul Yedida <rahul.yedida@pm.me>
2021-05-30 23:26:17 -0400

    Fix structure (#6)
    * Completed tasks
    
    * Implemented header component
    
    * Fixed header
    
    * Added tests, fixed minor bugs
    
    * Added test assets folder, implemented Footer, fixed Header positioning
    
    * Fix for test
    
    * Fix Cypress test
    
    * Fix Cypress test--does not expect https
    
    * My battle with Cypress continues...
    
    * Added tests for Footer component
    
    * Added title and button to home page
    
    * Added table, fixed footer spacing
    
    * Refactored tests, added tests for Home component
    
    * Refactored Home styles into styled component
    
    * Improved file structure
    
    * Removed unnecessary !important
    
    * Changed fixed width to padding
    
    * Refactored Header and Footer components to Styled Components
    
    * Bug fix in Main.js

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import setup from './testUtils';
+import setup from '../../testUtils';
 
 it('navigates to profy.dev/employers when left link is clicked', () => {
   setup('/');
---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 32px 80px;
+`;
+
+export const LinksList = styled.nav`
+  display: flex;
+  margin-top: 11px;
+  gap: 26px;
+`;
