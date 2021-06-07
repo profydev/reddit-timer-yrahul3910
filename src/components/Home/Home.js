5ab09112ab064c032a6e42d442f21f11753b4136
Rahul Yedida <rahul.yedida@pm.me>
2021-06-07 00:49:07 -0400

    Re-added content that got removed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,9 +9,27 @@ function Home() {
       <S.Subtitle>
         Great timing, great results! Find the best time to post on your subreddit.
       </S.Subtitle>
+
       <Link to="/search?q=javascript"><S.Button>Show me the best time</S.Button></Link>
       <p id="subreddit">r/javascript</p>
       <Link to="/search?q=javascript"><img alt="table" src="assets/table.svg" /></Link>
+
+      <S.Heading id="how-it-works">How it works</S.Heading>
+      <S.List>
+        <li>We find the 500 top posts from the past year for a subreddit.</li>
+        <li>The data is visualized in a heatmap grouped by weekday and hour of the day.</li>
+        <li>See immediately when to submit your reddit post.</li>
+      </S.List>
+
+      <S.Heading style={{ marginTop: '92px' }}>About</S.Heading>
+      <S.Paragraph>
+        This app was created during a course on
+        <Link to="https://profy.dev"> profy.dev </Link>
+        with the goal to implement a pixel&#8209;perfect real-world application with
+        professional workflows and tools like Kanban, ClickUp, Figma, GitHub, pull
+        requests and code reviews.
+        <Link to="https://profy.dev/employers"> Click here for more information.</Link>
+      </S.Paragraph>
     </S.Main>
   );
 }
